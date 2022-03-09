60a030583dd9d7dd22a7f9a2258c3b49c042680d
omsrg <siregar.om@gmail.com>
2022-03-09 19:44:33 +0700

    ready to deploy

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db';
@@ -13,5 +14,16 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
